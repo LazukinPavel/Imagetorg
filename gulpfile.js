a404b02d4600dd2a0afcf90ba37cf3be616324d2
LazukinPavel <lazukinpo@gmail.com>
2018-11-10 21:04:50 +0300

    Responsive layout
    Remaking layout for phones/tablets. Working on the header.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
+var 		gulp = require('gulp'),
+			sass = require('gulp-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
-	sourcemaps = require('gulp-sourcemaps'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	imagemin = require('gulp-imagemin'),
-	rename = require('gulp-rename'),
-	browserSync = require('browser-sync'),
-	del = require('del');
+	  sourcemaps = require('gulp-sourcemaps'),
+		  concat = require('gulp-concat'),
+		  uglify = require('gulp-uglify'),
+		imagemin = require('gulp-imagemin'),
+		  rename = require('gulp-rename'),
+	 browserSync = require('browser-sync'),
+			 del = require('del');
+		   cache = require('gulp-cache')
 	
 //converting sass into css
 gulp.task('sass', function(){ 
 	return gulp.src('src/assets/css/sass/**/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass())
-		.pipe(autoprefixer(['last 2 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(autoprefixer(['last 2 versions', '> 1%'], { cascade: true }))
 		.pipe(sourcemaps.write()) 
 		.pipe(gulp.dest('src/assets/css'))
 		.pipe(browserSync.reload({stream: true}));
@@ -34,11 +35,11 @@ gulp.task('minifyScripts', function() {
 //image optimization
 gulp.task('img', function() {
 	return gulp.src('src/assets/img/**/*')
-		.pipe(imagemin({
+		.pipe(cache(imagemin({
 			interlaced: true,
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
-	}))
+	})))
 });
 
 //browser sync
